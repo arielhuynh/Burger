e47598ba9dad4f99ab66d7a20d940dec39295bf3
Ariel Huynh <arielhuynh@ymail.com>
2020-05-02 11:02:29 -0700

    fix heroku

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,12 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-
-  });
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 
 module.exports = connection;
