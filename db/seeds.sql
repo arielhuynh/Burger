ae88edc80d9c6da5d813af3fedb11a31e2404d13
Ariel Huynh <arielhuynh@ymail.com>
2020-05-01 19:58:36 -0700

    added js and fixed issues

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vege Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crispy Chicken Burger', false);
